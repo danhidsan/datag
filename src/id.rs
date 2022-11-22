8b61c5b0baa7c0a74a31465da4b9e76439e95c52
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2022-11-22 21:02:17 +0100

    feat: generate_nie function

--- src/id.rs
+++ src/id.rs
@@ -1,26 +1,43 @@
 use rand::{self, Rng};
 
-fn calculate_nif_letter(nif_number: &i32) -> char {
+const NIF_RAND_START: i32 = 10000000;
+const NIF_RAND_END: i32 = 100000000;
+
+const NIE_RAND_START: i32 = 1000000;
+const NIE_RAND_END: i32 = 10000000;
+
+fn calculate_letter(number: &i32) -> char {
     let letters = "TRWAGMYFPDXBNJZSQVHLCKE".to_string();
-    let letter = letters.chars().nth(nif_number % 23).unwrap();
+    let letter = letters.chars().nth((number % 23).try_into().unwrap()).unwrap();
     
     letter
 }
 
 pub fn generate_nif() {
-    let x: i32 = 10;
-    let range_start = x.pow(7);
-    let range_end = x.pow(8) - 1;
-
     let mut rng = rand::thread_rng();
-    let rand_number = rng.gen_range(range_start..range_end);
-    let nif_letter = calculate_nif_letter(&rand_number);
+    let rand_number = rng.gen_range(NIF_RAND_START..NIF_RAND_END);
+    let nif_letter = calculate_letter(&rand_number);
 
     println!("{}{}", rand_number, nif_letter);
 }
 
 pub fn generate_nie() {
-    println!("NIE created");
+    let mut rng = rand::thread_rng();
+    let initial_random = rng.gen_range(0..3);
+
+    let nie_rand_number = rng.gen_range(NIE_RAND_START..NIE_RAND_END);
+
+    let complete_number: i32 = format!("{}{}", initial_random, nie_rand_number).parse().unwrap();
+
+    let end_letter = calculate_letter(&complete_number);
+
+    if initial_random == 0 {
+        println!("X{}{}", nie_rand_number, end_letter);
+    } else if initial_random == 1 {
+        println!("Y{}{}", nie_rand_number, end_letter);
+    } else if initial_random == 2 {
+        println!("Z{}{}", nie_rand_number, end_letter);
+    }
 }
 
 pub fn generate_cif() {
